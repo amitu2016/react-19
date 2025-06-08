7fa6bd0aff8f8fb6b964dc1ad5ce7004038cfbee
Amit Upadhyay <amitu2016@gmail.com>
2025-06-08 17:49:56 +0530

    props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import './App.css';
 import Product from './components/Product';
 
 function App() {
+
+  const products = [
+        {
+        name: "Apple",
+        img: "/images/apple.png",
+        code: "0001",
+        price: 12
+    },
+     {
+        name: "Banana",
+        img: "/images/banana.png",
+        code: "0002",
+        price: 8
+    },
+     {
+        name: "Orange",
+        img: "/images/orange.jpg",
+        code: "0003",
+        price: 10
+    }
+]
+
   return (
     <>
         <h1>Products</h1>
     <div className='product-list'>
-      <Product/>
+      {products.map((product) => (
+        <Product product={product}/>
+      ))}
     </div>
     </>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,39 +1,13 @@
-const Product = () => {
-    const products = [
-        {
-        name: "Apple",
-        img: "/images/apple.png",
-        code: "0001",
-        price: 12
-    },
-     {
-        name: "Banana",
-        img: "/images/banana.png",
-        code: "0002",
-        price: 8
-    },
-     {
-        name: "Orange",
-        img: "/images/orange.jpg",
-        code: "0003",
-        price: 10
-    }
-]
-
+const Product = ({ product }) => {
     return( 
-    <>
-        {products.map(
-        (product) => (
+   
         <div className="card" key={product.code}> 
             <img src={product.img} alt={product.name} width={120} height={120}/>
             <span>{product.code}</span>
             <span>{product.name}</span>
             <span>${product.price}</span>
             </div>
-       ))}
-   </>
-        
-);
+        );
 }
 
 export default Product;
